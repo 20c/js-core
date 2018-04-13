8c4acefc1b0d2a34fcfcdd92ec6114b9d8edc3a2
Stefan Pratter <stefan@20c.com>
2018-04-13 09:34:11 +0000

    snippet for dynamic loading of twentyc

---
+++ snippets/dynamic-load.js
@@ -0,0 +1,11 @@
+(function(url, onload){
+  if(!window.twentyc && !window.twentyc_loading) {
+    window.twentyc_loading=true;
+    var script = document.createElement("script");
+    script.src = url;
+    script.onload = onload;
+    document.head.appendChild(script);
+  }
+})("twentyc.core.js", function() {
+  // twentyc is now available
+})
